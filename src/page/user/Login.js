8d37bb59490b3164df5f5073b14b1b748d734b02
김민지 <wehouse201@naver.com>
2023-03-20 11:44:12 +0900

    login

--- src/page/user/Login.js
+++ src/page/user/Login.js
@@ -6,6 +6,17 @@ function Login() {
     <>
     <L.bg>
       <L.box>
+
+        <L.main>
+          <L.contents>
+            <L.header>LOGIN</L.header>
+            <L.detail placeholder="ID" type='text'/>
+            <L.detail placeholder="PASSWORD" type='password'/>
+            <L.go>LOGIN</L.go>
+            <L.text>아직 회원이 아니라면? <L.move>회뭔가입</L.move></L.text>
+          </L.contents>
+        </L.main>
+
         <L.sidebar>
           <L.icon/>
           <L.sidelogo>SOGOFORM</L.sidelogo>
--- src/styled/login.js
+++ src/styled/login.js
@@ -20,6 +20,71 @@ export const box=styled.div`
   backdrop-filter: blur(20px);
 `
 
+export const main=styled.div`
+  width:800px;
+  height:100%;
+  position: absolute;
+  border-radius:20px 0px 0px 20px;
+`
+
+export const contents=styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+
+export const header=styled.h2`
+  color:#fff;
+  margin-bottom:50px;
+`
+
+export const detail=styled.input`
+  width:600px;
+  height:60px;
+  border-radius:16px;
+  background: rgba(255, 255, 255, 0.9);
+  border:none;
+  padding:20px;
+  font-size:18px;
+  margin-bottom:20px;
+  &:focus{
+    outline:none;
+    background-color:#fff;
+  }
+`
+
+export const go = styled.button`
+  width:300px;
+  height:70px;
+  background-color:#74E9A3;
+  color:#fff;
+  border:none;
+  border-radius:10px;
+  font-size:20px;
+  margin:10px 0px 6px 0px;
+  &:focus{
+    background-color:#83FFB4;
+  }
+`
+
+export const move = styled.a`
+  text-decoration:none;
+  color:#fff;
+  &:hover{
+    color:#74E9A3;
+  }
+  &:focus{
+    color:#74E9A3;
+  }
+`
+
+export const text = styled.p`
+  color:#fff;
+  font-size:14px;
+`
+
+
 export const sidebar=styled.div`
   width: 300px;
   height:100%;
@@ -35,7 +100,7 @@ export const icon=styled.div`
   background-color:#fff;
   border-radius:100%;
   position:absolute;
-  top:44%;
+  top:48%;
   left:50%;
   transform:translate(-50%, -50%);
   background-image:url(${iconbg});
@@ -47,6 +112,6 @@ export const sidelogo=styled.p`
   position:absolute;
   bottom:20px;
   left:50%;
-  transform:translate(-50%)
+  transform:translate(-50%);
 `
 
