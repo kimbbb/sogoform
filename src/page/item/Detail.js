1230c5a4c5ac72870c1d63418a740b40289ad8d4
김민지 <wehouse201@naver.com>
2023-04-03 16:20:08 +0900

    detail

--- src/page/item/Detail.js
+++ src/page/item/Detail.js
@@ -1,13 +1,48 @@
 import Header from "../../component/Header";
 import * as D from "../../styled/detail";
-import img from "../../images/footer.png"
+import back from "../../images/close.png"
+import { useNavigate } from "react-router-dom";
 
 function Detail(){
+
+  let nav = useNavigate();
+
   return(
     <>
     <D.box>
-      <D.title>제목</D.title>
+      <D.title>제목이지롱
+        <D.close src={back} onClick={()=>{nav('/buy')}}/>
+      </D.title>
+      <D.innerbox>
+        <D.inbox>
+          <D.top>
+            <D.imgbox>
+              <D.topimg/>
+            </D.imgbox>
 
+            <D.cotent>
+              
+            </D.cotent>
+          </D.top>
+
+          <D.bottom>
+            <D.contenttext>입금 정보
+              <D.copy>copy</D.copy>
+            </D.contenttext>
+            <D.bank>카카오뱅크 7777010840188 김민지</D.bank>
+
+            <D.contenttext>주문자 정보</D.contenttext>
+            <D.information placeholder="이름"/>
+            <D.information placeholder="학번 ex) 3201"/>
+            <D.information placeholder="연락처 ex) 010 7101 4619"/>
+
+            <D.contenttext>기타 요청 사항</D.contenttext>
+            <D.information placeholder="기타 요청 사항 입력 (최대 30자 입력)"/>
+
+            <D.submit>폼 제출</D.submit>
+          </D.bottom>
+        </D.inbox>
+      </D.innerbox>
     </D.box>
     <Header/>
 
--- src/styled/detail.js
+++ src/styled/detail.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const box=styled.div`
   width:100%;
-  height:100vh;
+  height:90vh;
   position:absolute;
   top:80px;
 `
@@ -19,27 +19,108 @@ export const title = styled.div`
   align-items:center;
 `
 
-// export const content=styled.div`
-//   width:50%;
-//   height:100%;
-//   background-color:skyblue;
-//   position:absolute;
-//   left:50%;
-//   transform:translate(-50%);
-// `
+export const close = styled.img`
+  width:40px;
+  height:40px;
+  position:absolute;
+  left:15%;
+  cursor: pointer;
+`
 
-// export const goods=styled.div`
-//   width:100%;
-//   height:80%;
-//   border:1px solid #000;
-//   margin-top:20px;
-//   padding:10px;
-// `
-// export const goodsimg=styled.img`
-//   max-width:100%;
-//   min-width:80%;
-//   max-height:70%;
-//   background-color:yellow;
-// `
+export const innerbox = styled.div`
+  width:100%;
+  height:90%;
+  overflow:auto;
+  margin-top:10px;
+  padding:30px;
+  display:flex;
+  justify-content:center;
+`
+
+export const inbox = styled.div`
+  width:50%;
+  min-width:500px;
+`
+
+export const top = styled.div`
+  width:100%;
+  border:1px solid #000;
+  padding:20px;
+`
+
+export const imgbox = styled.div`
+  width:100%;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+`
+export const topimg = styled.img`
+  max-width:100%;
+  max-height:100%;
+  min-height:20%;
+`
+
+export const cotent = styled.div`
+  width:100%;
+  height:38%;
+  margin-top:2%;
+  overflow:auto;
+`
+
+export const bottom = styled.div`
+  width:100%;
+  height:550px;
+  padding-top:10px;
+`
+
+export const contenttext = styled.div`
+  font-size:22px;
+  font-weight:800;
+  margin:16px 0px 10px 0px;
+  display: flex;
+  align-items: center;
+`
+
+export const copy=styled.button`
+  width:60px;
+  height:30px;
+  background-color:#74E9A3;
+  font-size:14px;
+  color:#fff;
+  border:none;
+  border-radius:6px;
+  margin-left:10px;
+`
+
+
+export const bank = styled.p`
+  margin:0px;
+`
+
+export const information=styled.input`
+  width:100%;
+  height:50px;
+  border-radius:6px;
+  border:1px solid #828282;
+  margin-bottom:8px;
+  padding:10px;
+  &:focus{
+    outline:none;
+    border:2px solid #74E9A3;
+  }
+`
+
+export const submit = styled.button`
+  width:100%;
+  height:60px;
+  background-color:#74E9A3;
+  border:none;
+  border-radius:14px;
+  margin-top:30px;
+  font-size:18px;
+  font-weight:600;
+  color:#fff;
+`
+
 
 
